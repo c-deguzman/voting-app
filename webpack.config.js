f42effb56c6d0f47616e2984dbce0534e098be7e
commando5671 <commando5671@users.noreply.github.com>
2017-07-28 23:56:49 -0400

    Changed dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot-loader', 'babel-loader'],
+      loaders: ['react-hot-loader', 'babel'],
       include: path.join(__dirname, 'src')
     }]
   }
